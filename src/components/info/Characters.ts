47eed96b323bb9581044ddda2c97dda8dd85b163
SSEELLLLEERRSSDD <116637072+SSEELLLLEERRSSDD@users.noreply.github.com>
2024-07-04 22:24:58 +0600

    Add files via upload

---
+++ src/components/info/Characters.ts
@@ -0,0 +1,72 @@
+import D3 from '../../assets/3dman442.jpg'
+import B8 from '../../assets/8-ball.jpg'
+import AMIR from '../../assets/1130amr_ons_crd_01.jpg'
+import AD from '../../assets/231ad2_com_crd_01.jpg'
+import AEG from '../../assets/233aeg_com_crd_01.jpg'
+import SP from '../../assets/005smp_ons_crd_02.jpg'
+
+export const characters = [
+    {
+        id: '0',
+        Image: D3,
+        title: '3-D MAN',
+        description: 'The 3-D Man was a 1950s hero who came about through the unique merger of two brothers, Hal and Chuck Chandler. Chuck was a test pilot who was abducted by alien Skrulls during an important test flight. Earth was seen as a strategic location in the ongoing conflict between the alien Kree and Skrull Empires, so the Skrulls were seeking information on Earths space program and had captured Chuck to interrogate him. Chuck resisted and escaped, accidentally causing the explosion of the Skrull spacecraft in the process. While his brother Hal watched, the radiation from the explosion seemingly disintegrated Chuck, who disappeared in a burst of light. Hal later discovered, however, that the light burst had imprinted an image of Chuck on each lens of Hals eyeglasses. Through concentration, Hal could merge the images and cause Chuck to reappear as a three-dimensional man. Chuck become the costumed adventurer known as the 3-D Man and single-handedly subverted the Skrullsearly attempts to undermine Earthly civilization.',
+    },
+    {
+        id: '1',
+        Image: B8,
+        title: '8-BALL',
+        description: 'Jeff Hagees was once a designer of missile propulsion systems for a defense contractor. To cope with the stress of his job, he turned to gambling, losing thousands in pool rooms. When he fell into serious financial jeopardy, his employers took notice of his gambling problem, and fired him, fearing he was selling defense secrets to pay his debts. Furious, he designed the costumed identity of 8-Ball, basing his custom pool cue on his propulsion system work. He organized a gang of henchmen and went on a crime spree in New York city to make his fortune.',
+    },
+    {
+        id: '2',
+        Image: AMIR,
+        title: 'AAMIR KHAN',
+        description:'Aamir Khan is dedicated to his faith and family, and especially to his younger sister Kamala, AKA Ms. Marvel.',
+    },
+    {
+        id: '3',
+        Image: AD,
+        title: 'ADAM II',
+        description: 'Adam II was an android life-form originally created by Phinneas T. Horton, the creator of the original Human Torch. Adam II was Hortons attempt to replace and improve upon the Torch, who had abandoned his master to join the Invaders fight against the Axis powers of World War II. Ironically, it was Torchs first return to his creator that caused the team to stumble onto Adam II. The android had imprisoned his creator and made his own gang of mindless androids to help him conquer humanity. His initial plan was to replace then-Senate candidate John F. Kennedy with an android duplicate. The Invaders defeated Adam II, but not before the villains thugs crushed William Naslund (the second Captain America) to death.',
+    },
+    {
+        id: '4',
+        Image: AEG,
+        title: 'AEGIS', 
+        description: 'The complete history of Aegis is unknown. She has existed in this universe since its infancy. She is a proemial being. It is said that she prepared what exist for what is to come, by eliminating early lifeforms such as the Chaos Mites. It is documented that ages ago she was known and feared as Aegis (Lady of All Sorrows). Time has erased nearly all of her evil. In an early battle of the Gods Tenebrous and Aegis were among the only remaining survivors of a brutal battle with Galactus. They were imprisoned in the prison planetoid known as the Kyln by the World Devourer Galactus. The Kyln is located at the edge of the universe in a sector known as the Crunch Cascade. They remained locked in there for centuries.',
+    },
+    {
+        id: '5',
+        Image: SP,
+        title: 'SPIDER-MAN', 
+        description: 'Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.',
+    },
+];
+
+export const charactersLinks = [
+    {
+        title: 'Капитан Америка',
+        link: 'https://marvel.fandom.com/ru/wiki/Стивен_Роджерс_(199999)',
+    },
+    {
+        title: 'Доктор Стрэндж',
+        link: 'https://marvel.fandom.com/ru/wiki/Стефан_Стрэндж_(199999)',
+    },
+    {
+        title: 'Халк',
+        link: 'https://marvel.fandom.com/ru/wiki/Брюс_Беннер_(199999)',
+    },
+    {
+        title: 'Железный Человек',
+        link: 'https://marvel.fandom.com/ru/wiki/Энтони_Старк_(199999)',
+    },
+    {
+        title: 'Человек-Паук',
+        link: 'https://marvel.fandom.com/ru/wiki/Питер_Паркер_(199999)',
+    },
+    {
+        title: 'Тор',
+        link: 'https://marvel.fandom.com/ru/wiki/Тор_Одинсон_(199999)',
+    },
+];
